c6b4f406685a1f725b3c5371ca689ad2576bc79c
Chris LoJacono <clojacono92@gmail.com>
2021-04-09 20:23:18 -0500

    act

---
+++ TheJosephusProblemTDD/LastPersonStanding.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheJosephusProblemTDD
+{
+    public class LastPersonStanding
+    {
+        public int LastPersonStanding(int numberOfPeople, int killingInterval)
+        {
+
+        }
+    }
+}
