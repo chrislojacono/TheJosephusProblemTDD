cb360afbb75f106f06138a024600edede04a7c09
Chris LoJacono <clojacono92@gmail.com>
2021-04-09 21:06:45 -0500

    PASSSSEEDDD

--- TheJosephusProblemTDD/LastPersonStanding.cs
+++ TheJosephusProblemTDD/LastPersonStanding.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace TheJosephusProblemTDD
@@ -8,7 +9,28 @@ namespace TheJosephusProblemTDD
     {
         public int JosephusCalculator(int numberOfPeople, int killingInterval)
         {
-            return 1;
+            var peopleToLoopOver = Enumerable.Range(1, numberOfPeople).ToList();
+
+            var originalKillingInterval = killingInterval - 1;
+            
+            while (peopleToLoopOver.Count > 1)
+            {
+                for (int i = killingInterval - 1; peopleToLoopOver.Count > 1; i += originalKillingInterval)
+                {
+                    if(i > peopleToLoopOver.Count - 1)
+                    {
+                        killingInterval = i - peopleToLoopOver.Count + 1;
+                        break;
+                    }
+                    peopleToLoopOver.RemoveAt(i);
+
+                }
+
+            }
+            var lastGuyStanding = peopleToLoopOver[0];
+
+
+            return lastGuyStanding;
         }
     }
 }
