ccafa1bc1923300855ef1960bdf8cdcc6d19cff1
Chris LoJacono <clojacono92@gmail.com>
2021-04-10 08:51:49 -0500

    theory version

--- TheJosephusProblem.tests/TheoryVersion.cs
+++ TheJosephusProblem.tests/TheoryVersion.cs
@@ -6,10 +6,18 @@ using TheJosephusProblemTDD;
 
 namespace TheJosephusProblem.tests
 {
-    class TheoryVersion
+    public class TheoryVersion
     {
         [Theory]
         [InlineData(7, 2, 7)]
-        public void Find_the_last_man_standing(int numberOfPeople)
+        [InlineData(10, 3, 4)]
+        public void Find_the_last_man_standing(int numberOfPeople, int killingInterval, int expectedResult)
+        {
+            var lastPersonStanding = new LastPersonStanding();
+
+            var lastOneAliveResult = lastPersonStanding.JosephusCalculator(numberOfPeople, killingInterval);
+
+            Assert.Equal(expectedResult, lastOneAliveResult);
+        }
     }
 }
